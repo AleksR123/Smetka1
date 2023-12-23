84705419f9a6d93b0957b73a23b41a0880af4e52
unknown <aleksandarristov28@gmail.com>
2023-12-23 14:23:49 +0100

    first commit

---
+++ src/Smetka.java
@@ -0,0 +1,22 @@
+
+public class Smetka {
+
+		public static void main(String[] args) {
+			int jabolki = 32;
+			int kompir = 20;
+			double pras = 50;
+			int rez1, rez2; 
+			double rezultat, rez3;
+			rez1 = jabolki * 2;
+			rez2 = kompir * 5;
+			rez3 = pras * 2.5;
+			rezultat = rez1 + rez2 + rez3;
+			
+			System.out.println(rezultat);
+			System.out.println("denari");
+
+		}
+
+	}
+
+
